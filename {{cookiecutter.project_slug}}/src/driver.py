4722085ca925a13515e535c94faa9b96c66431c3
johnathanvidu <johnathan.vidu@gmail.com>
2018-02-19 13:36:37 +0200

    adding custom service template

---
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -0,0 +1,28 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
+    AutoLoadAttribute, AutoLoadDetails, CancellationContext
+#from data_model import *  # run 'shellfoundry generate' to generate data model classes
+
+
+class {{cookiecutter.driver_name}} (ResourceDriverInterface):
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called everytime a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        pass
+
+    def cleanup(self):
+        """
+        Destroy the driver session, this function is called everytime a driver instance is destroyed
+        This is a good place to close any open sessions, finish writing to log files
+        """
+        pass
